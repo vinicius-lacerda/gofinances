c36dddd9735b8e83763e768db4ad7a729a9dd0dd
Vinícius Lacerda <viniciuss.sync@gmail.com>
2022-01-09 18:49:13 -0400

    Changing TouchableOpacity from RectButton

--- src/components/Forms/TransactionTypeButton/index.tsx
+++ src/components/Forms/TransactionTypeButton/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
 import { 
     Container,
     Icon,
-    Title
+    Title,
+    Button,
 } from './styles';
 
 const icons = {
@@ -12,7 +13,7 @@ const icons = {
     down: 'arrow-down-circle'
 }
 
-interface Props extends TouchableOpacityProps{
+interface Props extends RectButtonProps{
     type: 'up' | 'down';
     title: string;
     isActive: boolean;
@@ -21,17 +22,21 @@ interface Props extends TouchableOpacityProps{
 export function TransactionTypeButton({ type, title, isActive, ...rest} : Props){
     return (
         <Container 
-        {...rest} 
-        isActive={isActive}
-        type={type}
-        >
-            <Icon 
-            name={icons[type]} 
+            
+            isActive={isActive}
             type={type}
-            />
-            <Title>
-                {title}
-            </Title>
+        >
+            <Button
+                {...rest} 
+            >
+                <Icon 
+                name={icons[type]} 
+                type={type}
+                />
+                <Title>
+                    {title}
+                </Title>
+            </Button>
 
         </Container>
     )
