c1b06b99706b892f135d0c27f1f9a3d4d1065907
Vinícius Lacerda <viniciuss.sync@gmail.com>
2022-02-06 12:15:23 -0400

    feature/Add Graphics

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from 'victory-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import { useTheme } from 'styled-components';
 import { HistoryCard } from '../../components/HistoryCard';
 
-import { Container, Header, Title } from './styles';
+import { 
+    Container, 
+    Header, 
+    Title,
+    Content,
+    ChartContainer
+} from './styles';
+import { categories } from '../../utils/categories';
+
+interface TransactionData {
+    type: 'positive' | 'negative';
+    name: string;
+    amount: string;
+    category: string;
+    date: string;
+}
+
+interface CategoryData {
+    name: string;
+    total: number;
+    totalFormatted: string;
+    color: string;
+    percent: string;
+}
 
 export function Resume() {
+    const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+    const theme = useTheme();
+
+    async function loadData() {
+        const dataKey = '@gofinances:transactions';
+        const response = await AsyncStorage.getItem(dataKey);
+        const responseFormatted = response ? JSON.parse(response) : [];
+
+        const expensives = responseFormatted
+        .filter((expensive: TransactionData) => expensive.type === 'negative');
+
+        const expensivesTotal = expensives
+        .reduce((acumullator: number, expensive: TransactionData) => {
+            return acumullator + Number(expensive.amount);
+        }, 0);
+
+        const totalByCategory: CategoryData[] = [];
+
+        categories.forEach(category => {
+            let categorySum = 0;
+
+            expensives.forEach((expensive: TransactionData) => {
+                if(expensive.category === category.key){
+                    categorySum += Number(expensive.amount);
+                }
+            });
+
+            if(categorySum > 0) {
+                const totalFormatted = categorySum
+                .toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+
+                const percent = `${(categorySum / expensivesTotal * 100).toFixed(0)}%`;
+
+                totalByCategory.push({
+                    name: category.name,
+                    color: category.color,
+                    total: categorySum,                    
+                    totalFormatted,
+                    percent
+                });
+            }
+
+
+        });
+
+        setTotalByCategories(totalByCategory);
+
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
     return (
         <Container>
             <Header>
@@ -12,11 +97,33 @@ export function Resume() {
                 </Title>
             </Header>
 
-            <HistoryCard 
-                title="Compras"
-                amount="R$150,00"
-                color="red"
-            />
+            <Content>
+                <ChartContainer>
+                    <VictoryPie 
+                        data={totalByCategories}
+                        colorScale={totalByCategories.map(category => category.color)}
+                        style={{
+                            labels: {
+                                fontSize: RFValue(18),
+                                fontWeight: 'bold',
+                                fill: theme.colors.shape
+                            }
+                        }}
+                        labelRadius={50}
+                        x="percent"
+                        y="total"
+                    />
+                </ChartContainer>
+                {
+                totalByCategories.map(item => (
+                        <HistoryCard 
+                            title={item.name}                        
+                            color={item.color}
+                            amount={item.totalFormatted}
+                        />
+                    ))
+                }
+            </Content>
         </Container>
     )
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -23,3 +23,12 @@ export const Title = styled.Text`
 
     font-size: ${RFValue(18)}px;
 `;
+
+export const Content = styled.ScrollView.attrs({
+    contentContainerStyle: { padding: 24 }
+})``;
+
+export const ChartContainer = styled.View`
+    width: 100%;
+    align-items: center;
+`;
